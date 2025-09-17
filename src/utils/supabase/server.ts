5d222bdeba502866cf1c39f5a8d5a9196065c224
FrostFoe <frostfoe@gmail.com>
2025-09-17 11:20:30 +0000

    studio-2073467364:~/studio{main}$ npm run build
    > nextjs-starter-kit@0.

--- src/utils/supabase/server.ts
+++ src/utils/supabase/server.ts
@@ -1,18 +1,17 @@
 import { createServerClient as createSupabaseServerClient } from '@supabase/ssr';
 import { cookies } from 'next/headers';
-import type { ReadonlyRequestCookies } from 'next/dist/server/web/spec-extension/adapters/request-cookies';
 
-export async function createClient(cookieStore?: ReadonlyRequestCookies) {
-  const store = cookieStore || cookies();
+export function createClient() {
+  const cookieStore = cookies();
 
   return createSupabaseServerClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!, {
     cookies: {
       get(name: string) {
-        return store.get(name)?.value;
+        return cookieStore.get(name)?.value;
       },
       set(name: string, value: string, options) {
         try {
-          store.set({ name, value, ...options });
+          cookieStore.set({ name, value, ...options });
         } catch (error) {
           // The `set` method was called from a Server Component.
           // This can be ignored if you have middleware refreshing
@@ -21,7 +20,7 @@ export async function createClient(cookieStore?: ReadonlyRequestCookies) {
       },
       remove(name: string, options) {
         try {
-          store.set({ name, value: '', ...options });
+          cookieStore.set({ name, value: '', ...options });
         } catch (error) {
           // The `delete` method was called from a Server Component.
           // This can be ignored if you have middleware refreshing
