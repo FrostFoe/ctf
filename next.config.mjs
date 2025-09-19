c0d6c6fbabb2c4589da7a800bc1140c8526dcb38
FrostFoe <frostfoe@gmail.com>
2025-09-19 04:42:34 +0000

    Try fixing this error: `Runtime Error: Invalid src prop (https://picsum.

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
   images: {
-    domains: ['cdn.simpleicons.org', 'localhost', 'paddle-billing.vercel.app'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+    ],
   },
 };
 
